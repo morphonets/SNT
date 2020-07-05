fd1b9ca155454b8a77d5b59dc0c1c1db7dc79f12
tferr <tiago.tferr@gmail.com>
2020-07-05 17:27:33 -0400

    Revert "Bump to next development cycle"
    This reverts commit c7057a5ab5801737443e3f55d2448bb1203dbf3f.

--- src/main/java/sc/fiji/snt/Tree.java
+++ src/main/java/sc/fiji/snt/Tree.java
@@ -1003,8 +1003,7 @@ public class Tree {
 		for (int z = 0; z < depth; ++z) {
 			slices_data[z] = (short[]) s.getPixels(destinationImp.getStackIndex(channel, z + 1, frame));
 		}
-		initPathAndFillManager();
-		pafm.setPathPointsInVolume(list(), slices_data, value, width, height, depth);
+		new PathAndFillManager().setPathPointsInVolume(list(), slices_data, value, width, height, depth);
 	}
 
 	/**
@@ -1531,25 +1530,7 @@ public class Tree {
 			it.next().setColor(colors[i]);
 		}
 	}
-
-	/**
-	 * Assigns the spatial calibration of an image to this Tree.
-	 *
-	 * @param imp the image providing the spatial calibration. Null allowed.
-	 */
-	public void assignImage(final ImagePlus imp) {
-		initPathAndFillManager();
-		Calibration cal;
-		if (imp == null) {
-			pafm.resetSpatialSettings();
-			cal = new Calibration();
-		} else {
-			pafm.assignSpatialSettings(imp);
-			cal = imp.getCalibration();
-		}
-		list().forEach(path -> path.setSpacing(cal));
-	}
-
+	
 	/**
 	 * Swaps the coordinates of two axes in this Tree.
 	 *
