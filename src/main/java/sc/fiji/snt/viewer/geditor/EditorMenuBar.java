61820ee49f48509ef39a9c602058c288e52debb0
Cameron Arshadi <arshadic@hhmi.org>
2020-08-05 00:58:21 -0400

    fix merge conflict

--- src/main/java/sc/fiji/snt/viewer/geditor/EditorMenuBar.java
+++ src/main/java/sc/fiji/snt/viewer/geditor/EditorMenuBar.java
@@ -452,7 +452,7 @@ public class EditorMenuBar extends JMenuBar
 		menu.add(editor.bind("Get Sinks", new AnalyzeGraph(AnalyzeType.GET_SINKS, aGraph, context)));
 		menu.add(editor.bind("Find Cycles", new AnalyzeGraph(AnalyzeType.FIND_CYCLES, aGraph, context)));
 		menu.addSeparator();
-		menu.add(editor.bind("Show Rec. Viewer", new EditorActions.ShowInViewer3DAction(context)));
+		menu.add(editor.bind("Show Rec. Viewer", new EditorActions.ShowInViewer3DAction(context.getService(CommandService.class))));
 	}
 
 	private GuiUtils getGuiUtils() {
