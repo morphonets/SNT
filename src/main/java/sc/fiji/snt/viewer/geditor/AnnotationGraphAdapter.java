9a7c1ef2d2dd8924ada08986d85b5c6ecde89fec
Cameron Arshadi <arshadic@hhmi.org>
2020-08-10 23:16:15 -0400

    AnnotationGraphAdapter: re-instate default vertex shape
    - A shape is necessary for vertex color-coding to work
    - use rounded rectangle

--- src/main/java/sc/fiji/snt/viewer/geditor/AnnotationGraphAdapter.java
+++ src/main/java/sc/fiji/snt/viewer/geditor/AnnotationGraphAdapter.java
@@ -43,7 +43,7 @@ public class AnnotationGraphAdapter extends SNTGraphAdapter<BrainAnnotation, Ann
         vertexStyle.put(mxConstants.STYLE_LABEL_POSITION, mxConstants.ALIGN_CENTER);
         vertexStyle.put(mxConstants.STYLE_VERTICAL_LABEL_POSITION, mxConstants.ALIGN_MIDDLE);
         vertexStyle.put(mxConstants.STYLE_ROUNDED, true);
-        vertexStyle.put(mxConstants.STYLE_SHAPE, mxConstants.NONE);
+        vertexStyle.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_RECTANGLE);
         vertexStyle.put(mxConstants.STYLE_FONTCOLOR, "black");
 
         if (verticesColor != null)
