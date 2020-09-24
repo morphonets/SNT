0e60d975f4b14a2e35fa371b2a35921f2872147a
tferr <tiago.tferr@gmail.com>
2020-09-23 22:18:35 -0400

    SkeletonConverterCmd: Standardize file header and javadoc string

--- src/main/java/sc/fiji/snt/plugin/SkeletonConverterCmd.java
+++ src/main/java/sc/fiji/snt/plugin/SkeletonConverterCmd.java
@@ -1,3 +1,25 @@
+/*-
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2010 - 2019 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 package sc.fiji.snt.plugin;
 
 import ij.ImagePlus;
@@ -6,6 +28,7 @@ import org.scijava.module.MutableModuleItem;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.widget.ChoiceWidget;
+
 import sc.fiji.snt.PathAndFillManager;
 import sc.fiji.snt.Tree;
 import sc.fiji.snt.analysis.SkeletonConverter;
@@ -15,7 +38,12 @@ import sc.fiji.snt.gui.cmds.ChooseDatasetCmd;
 import java.util.*;
 import java.util.stream.IntStream;
 
-
+/**
+ * Command providing a GUI for {@link SkeletonConverter}
+ *
+ * @author Cameron Arshadi
+ * @author Tiago Ferreira
+ */
 @Plugin(type = Command.class, visible = false, label="Tree(s) from Skeleton Image...", initializer = "init")
 public class SkeletonConverterCmd extends ChooseDatasetCmd {
 
