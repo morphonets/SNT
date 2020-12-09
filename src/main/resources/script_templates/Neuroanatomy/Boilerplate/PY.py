3c611b3dff906706e8949f0dfec099dd4166d217
tferr <tiago.tferr@gmail.com>
2020-12-08 23:28:03 -0500

    Jython boilerplate: Fix imports to reflect new API

--- src/main/resources/script_templates/Neuroanatomy/Boilerplate/PY.py
+++ src/main/resources/script_templates/Neuroanatomy/Boilerplate/PY.py
@@ -15,15 +15,15 @@ version:
 info:    
 """
 
-from sc.fiji.snt import (Path, PathAndFillManager, SNT, SNTUI, Tree)
+from sc.fiji.snt import Path, PathAndFillManager, SNT, SNTUI, Tree
 from sc.fiji.snt.analysis import *
 from sc.fiji.snt.analysis.graph import DirectedWeightedGraph
-from sc.fiji.snt.analysis.sholl import TreeParser
-from sc.fiji.snt.annotation import (AllenCompartment, AllenUtils, VFBUtils, ZBAtlasUtils)
-from sc.fiji.snt.io import (FlyCircuitLoader, MouseLightLoader, NeuroMorphoLoader)
-from sc.fiji.snt.plugin import (SkeletonizerCmd, StrahlerCmd)
-from sc.fiji.snt.util import (BoundingBox, PointInImage, SNTColor, SWCPoint)
-from sc.fiji.snt.viewer import (Annotation3D, OBJMesh, MultiViewer2D, Viewer2D, Viewer3D)
+from sc.fiji.snt.analysis.sholl.parsers import TreeParser
+from sc.fiji.snt.annotation import AllenCompartment, AllenUtils, VFBUtils, ZBAtlasUtils
+from sc.fiji.snt.io import FlyCircuitLoader, MouseLightLoader, NeuroMorphoLoader
+from sc.fiji.snt.plugin import SkeletonizerCmd, StrahlerCmd
+from sc.fiji.snt.util import BoundingBox, PointInImage, SNTColor, SWCPoint
+from sc.fiji.snt.viewer import Annotation3D, OBJMesh, MultiViewer2D, Viewer2D, Viewer3D
 
 # Documentation Resources: https://imagej.net/SNT:_Scripting
 # Latest SNT API: https://morphonets.github.io/SNT/
