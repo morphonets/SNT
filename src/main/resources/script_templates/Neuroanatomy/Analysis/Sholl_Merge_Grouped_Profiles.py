87872ac5e14a6f5bec9113b9dcd2044cb4f052c7
tferr <tiago.tferr@gmail.com>
2021-06-28 22:32:41 -0400

    Add integration options to Sholl_Merge_Grouped_Profiles

--- src/main/resources/script_templates/Neuroanatomy/Analysis/Sholl_Merge_Grouped_Profiles.py
+++ src/main/resources/script_templates/Neuroanatomy/Analysis/Sholl_Merge_Grouped_Profiles.py
@@ -9,6 +9,7 @@
 # @double(label="Starting radius") sradius
 # @boolean(label="Impose radius step size", min=0, description="<html>If selected, merged profiles will have the radius step size specified below") impose_step_size
 # @double(label="Radius step size", min=0) step_size
+# @String(label="Integration method", choices={"Mean","Median","Max"}, description="<html>If multiple measurements exist within the specified step size, how should they be integrated?") integration
 # @boolean(label="Impose end radius", description="<html>If selected, merged profiles will end at the radius specified below") impose_eradius
 # @double(label="Ending radius",min=0) eradius
 # @boolean(label="Fit averaged profile", description="<html>Apply a 'best-fit' polynomal regression to averaged profile?") fit_avg
@@ -19,15 +20,13 @@
 
 import csv, glob, math, os
 from collections import OrderedDict
-from java.lang import IllegalArgumentException
 from sc.fiji.snt.analysis import SNTTable
 from sc.fiji.snt.analysis.sholl import Profile
 from sc.fiji.snt.analysis.sholl.gui import ShollPlot
 from sc.fiji.snt.analysis.sholl.math import LinearProfileStats
 
 
-
-def log(msg, level = "info"):
+def log(msg, level="info"):
     # https://forum.image.sc/t/logservice-issue-with-jython-slim-2-7-2-and-scripting-jython-1-0-0/
     from org.scijava.log import LogLevel
     if "warn" in level:
@@ -37,11 +36,9 @@ def log(msg, level = "info"):
     else:
         lservice.log(LogLevel.INFO, msg)
 
-
 def error(msg):
     """ Displays an error message """
     uiservice.showDialog(msg, "Error")
-
 
 def ss(data, mean):
     """ Returns the sum of square deviations
@@ -49,7 +46,6 @@ def ss(data, mean):
     """
     ss = sum((x-mean)**2 for x in data)
     return ss
-
 
 def stdev(data, mean):
     """Calculates the (population) standard deviation"""
@@ -60,7 +56,13 @@ def stdev(data, mean):
     svar = ssd/(n)
     return svar**0.5
 
-
+def median(data):
+    """Calculates the median of a list"""
+    n = len(data)
+    index = n // 2
+    if n % 2:
+        return sorted(data)[index]
+    return sum(sorted(data)[index - 1:index + 1]) / 2
 
 def addfit(plot, lstats):
     lstats.findBestFit(2, 50, 0.2, -1)
@@ -70,7 +72,6 @@ def addfit(plot, lstats):
         label = u'%s fit (R\u00B2=%.2f)' % (poly, rsqred)
         plot.setColor("red")
         plot.addPoints(lstats.getXvalues(), lstats.getFitYvalues(), label)
-
 
 def main():
 
@@ -82,7 +83,7 @@ def main():
     files = sorted(glob.glob(os.path.join(str(dir), glob_pattern)))
 
     if not files:
-        error("The directory %s\ndoes not contain files matching the specified"\
+        error("The directory %s\ndoes not contain files matching the specified"
               " pattern (or it does not exist)." % dir)
         return
 
@@ -116,7 +117,7 @@ def main():
             if not has_header:
                 log("Skipping... File has no column headings...")
                 continue
-            
+
             header_row = incsv.next()
             try:
                 xcolumn_idx = header_row.index(xcolumn_header)
@@ -138,11 +139,10 @@ def main():
                     continue
 
     if not all_data:
-        error("%s files were parsed but no valid data existed.\nPlease revise"\
-              " settings or check the Console for details.\nNote that column"\
+        error("%s files were parsed but no valid data existed.\nPlease revise"
+              " settings or check the Console for details.\nNote that column"
               " headings are case-sensitive." % len(files))
         return
-
 
     log("Assembling table with merged data...")
     table = SNTTable()
@@ -152,11 +152,11 @@ def main():
         for entry in entries:
             table.appendToLastRow(entry[0], entry[1])
     uiservice.show("MergedProfiles_Inputs", table)
-    
+
     log("Assembling stats...")
     # Define the number of intervals we'll be dealing with
     min_radius = sradius if impose_sradius and sradius >= 0 else min(all_data.keys())
-    max_radius = eradius if impose_eradius and eradius > 0 else max(all_data.keys()) 
+    max_radius = eradius if impose_eradius and eradius > 0 else max(all_data.keys())
     if not impose_step_size or step_size <= 0:
         log("Computing step size...")
         step_size = max([all_data.keys()[i+1]-all_data.keys()[i] for i in range(len(all_data.keys())-1)])
@@ -176,12 +176,16 @@ def main():
                     if value:
                         interval_inters.append(table.get(col_idx, row_idx))
             if interval_inters:
-                # if intersections existed in this range, stash the average of the intersections
-                # in the interval. This could be replaced with the median, mode, etc..
-                stats[bin_start].append(sum(interval_inters)/float(len(interval_inters)))
+                # if intersections existed in this range, integrate them in a single value
+                if integration == 'Median':
+                    stats[bin_start].append(median(interval_inters))
+                elif integration == 'Max':
+                    stats[bin_start].append(max(interval_inters))
+                else:
+                    stats[bin_start].append(sum(interval_inters)/float(len(interval_inters)))
 
     if not stats:
-        error("It was not possible to assemble statistical data.\n"\
+        error("It was not possible to assemble statistical data.\n"
               "Please revise settings.")
         return
 
@@ -195,14 +199,16 @@ def main():
         the_sum = sum(interval_means)
         the_avg = the_sum/len(interval_means)
         table.appendToLastRow("Radius (interval start)", bin_start)
-        table.appendToLastRow("Mean", the_avg)
+        table.appendToLastRow("Mean (of %ss)" % integration, the_avg)
         table.appendToLastRow("StDev", stdev(interval_means, the_avg))
         table.appendToLastRow("Sum", the_sum)
         table.appendToLastRow("N", len(interval_means))
     uiservice.show("MergedProfiles_Stats", table)
 
-    plot = ShollPlot("Mean Sholl Plot", xcolumn_header, ycolumn_header)
-    plot.setColor("cyan", "blue")
+    # Plot averaged profile. Add polynomial fit
+    plot = ShollPlot("Merged Sholl Plot", xcolumn_header, ycolumn_header)
+    plot.setLineWidth(2)
+    plot.setColor(("cyan" if nbins > 100 else "blue"), "blue")
     plot.addPoints(table.get(0), table.get(1), table.get(2), u'Mean\u00B1SD')
     if fit_avg:
         addfit(plot, LinearProfileStats(Profile(table.get(0), table.get(1))))
