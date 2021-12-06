morphonets/SNT

commit dc4fab157c00d1145cd40ce3267499842632d051

Code cleanup
formatting, switch statements w/o default, etc.

src/main/java/sc/fiji/snt/PathAndFillManager.java
@@ -2104,6 +2104,9 @@ public class PathAndFillManager extends DefaultHandler implements
 				if (plugin != null) plugin.updateTracingViewers(true);
 				resetListeners(null, true);
 				break;
+
+		default:
+			throw new IllegalArgumentException("Unrecognized field " + qName);
 		}
 
 	}

src/main/java/sc/fiji/snt/Path.java
@@ -134,7 +134,7 @@ public class Path implements Comparable<Path> {
 	 * NB: A regular path should have a path ID > 0, because if present, its fitted
 	 * version will have an ID of -path ID. see {@link #setFitted(Path)}
 	 */
-	private long id = -1l;
+	private long id = -1L;
 	private String treeLabel;
 
 	// NB: The leagacy 3D viewer requires always a unique name

src/main/java/sc/fiji/snt/analysis/ShollAnalyzer.java
@@ -91,7 +91,7 @@ public class ShollAnalyzer {
 	private final Tree tree;
 	private final LinkedHashMap<String, Number> metrics;
 	private final ArrayList<Double> maximaRadii;
-	private final ArrayList<double[]> secondaryMaxima;;
+	private final ArrayList<double[]> secondaryMaxima;
 
 	private NormalizedProfileStats nStats;
 	private LinearProfileStats lStats;

src/main/java/sc/fiji/snt/analysis/StrahlerAnalyzer.java
@@ -171,9 +171,9 @@ public class StrahlerAnalyzer {
 			final List<SWCPoint> children = Graphs.successorListOf(graph, node);
 			final int highestOrder = (int) Collections.max(children, Comparator.comparing(n -> (int) n.v)).v;
 			final long highestOrderFreq = children.stream().filter(c -> (int) c.v == highestOrder).count();
-			if (highestOrderFreq == 1l)
+			if (highestOrderFreq == 1L)
 				order = highestOrder;
-			else if (highestOrderFreq > 1l) {
+			else if (highestOrderFreq > 1L) {
 				order = highestOrder + 1;
 			}
 		}

src/main/java/sc/fiji/snt/gui/cmds/ComputeSecondaryImg.java
@@ -378,7 +378,7 @@ public class ComputeSecondaryImg<T extends RealType<T> & NativeType<T>, U extend
 							type,
 							op);
 				} else {
-					out = ops.create().img(in, type, new CellImgFactory<>(type));;
+					out = ops.create().img(in, type, new CellImgFactory<>(type));
 					op.compute(in, out);
 				}
 
@@ -612,6 +612,7 @@ public class ComputeSecondaryImg<T extends RealType<T> & NativeType<T>, U extend
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private void loadPreferences() {
 		// FIXME: Somehow values are not persisting!? Maybe because we are implementing Interactive? 
 		prefService.get(getClass(), "filter", TUBENESS);

src/main/java/sc/fiji/snt/plugin/ShollAnalysisTreeCmd.java
@@ -605,7 +605,7 @@ public class ShollAnalysisTreeCmd extends DynamicCommand implements Interactive,
 			Collection<Tree> trees = Tree.listFromFile(file.getAbsolutePath());
 			if (trees != null && trees.size() == 1) {
 				tree = trees.iterator().next();
-				multipleTreesExist = false;;
+				multipleTreesExist = false;
 			} else if (trees != null && trees.size() > 1) {
 				tree = null;
 				multipleTreesExist = true;

src/main/java/sc/fiji/snt/viewer/OBJMesh.java
@@ -430,7 +430,7 @@ public class OBJMesh {
 
 	private class OBJFilePlus extends OBJFile {
 
-		/**
+		/*
 		 * Copied ipsis verbis from
 		 * {@link org.jzy3d.io.obj.OBJFile#loadModelFromURL(URL)} but accommodates files
 		 * with trailing spaces //TODO: submit PR upstream
@@ -464,7 +464,6 @@ public class OBJMesh {
 						default:
 							break;
 						}
-						;
 					}
 					// post-process data
 					// free anything that ended up being unused
@@ -490,6 +489,7 @@ public class OBJMesh {
 							input.close();
 						}
 					} catch (final IOException closee) {
+						// ignore
 					}
 				}
 			} else {

src/main/java/sc/fiji/snt/viewer/Viewer3D.java
@@ -1183,7 +1183,7 @@ public class Viewer3D {
 	 */
 	public Annotation3D mergeAnnotations(final Collection<Annotation3D> annotations, final String label) {
 		final boolean updateFlag = viewUpdatesEnabled;
-		setSceneUpdatesEnabled(false);;
+		setSceneUpdatesEnabled(false);
 		annotations.forEach(annot -> {
 			final String[] labelAndManagerEntry = TagUtils.getUntaggedAndTaggedLabels(annot.getLabel());
 			removeDrawable(getAnnotationDrawables(), labelAndManagerEntry[0], labelAndManagerEntry[1]);
@@ -2321,8 +2321,9 @@ public class Viewer3D {
 			return MESH_LABEL_ALLEN;
 		case "zebrafish":
 			return MESH_LABEL_ZEBRAFISH;
+		default:
+			return null;
 		}
-		return null;
 	}
 
 	private Color getNonUserDefColor() {
@@ -3436,7 +3437,7 @@ public class Viewer3D {
 			sceneMenu.add(reload);
 			final JMenuItem rebuild = new JMenuItem(new Action(Action.REBUILD, KeyEvent.VK_R, true, true));
 			rebuild.setIcon(IconFactory.getMenuIcon(GLYPH.RECYCLE));
-			sceneMenu.add(rebuild);;
+			sceneMenu.add(rebuild);
 			JMenuItem wipe = new JMenuItem("Wipe Scene...", IconFactory.getMenuIcon(GLYPH.DANGER));
 			wipe.addActionListener(e -> wipeScene());
 			sceneMenu.add(wipe);
@@ -4045,7 +4046,7 @@ public class Viewer3D {
 				}
 				(new getMeshColors()).execute();
 			});
-			menu.add(mi);;
+			menu.add(mi);
 
 			// Mesh customizations
 			mi = new JMenuItem("Color...", IconFactory.getMenuIcon(

commit 1b5eb7eb773d0192ffad46608ea1b759cd0fbce0

Fix XML parsing
(bug introduced with dc4fab157c00 cleanup

src/main/java/sc/fiji/snt/PathAndFillManager.java
@@ -2106,7 +2106,7 @@ public class PathAndFillManager extends DefaultHandler implements
 				break;
 
 		default:
-			throw new IllegalArgumentException("Unrecognized field " + qName);
+			// parsing samplespacing, etc.
 		}
 
 	}