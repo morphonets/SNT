8a44b45ca0e323dea489404f159b102e6ca294d9
Tiago Ferreira <tiago.tferr@gmail.com>
2022-07-21 12:39:18 -0400

    RemoteSWCLoader: Add convenience method for downloading SWCs

--- src/main/java/sc/fiji/snt/io/RemoteSWCLoader.java
+++ src/main/java/sc/fiji/snt/io/RemoteSWCLoader.java
@@ -23,7 +23,17 @@
 package sc.fiji.snt.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
+import org.apache.commons.io.IOUtils;
+
+import sc.fiji.snt.SNTUtils;
 import sc.fiji.snt.Tree;
 
 /**
@@ -40,5 +50,23 @@ public interface RemoteSWCLoader {
 	public BufferedReader getReader(final String cellId);
 
 	public Tree getTree(final String cellId);
+
+	static boolean download(final String urlString, final File destinationFile) {
+		try {
+			final BufferedReader br = new BufferedReader(new InputStreamReader(new URL(urlString).openStream()));
+			final BufferedWriter bw = Files.newBufferedWriter(destinationFile.toPath(), StandardCharsets.UTF_8);
+//			final char[] buffer = new char[8192];
+//			int n;
+//			while (-1 != (n = br.read())) {
+//				bw.write(buffer, 0, n);
+//			}
+//			br.close();
+//			bw.close();
+			return IOUtils.copy(br, bw) > 0 && destinationFile.exists() && destinationFile.length() > 0;
+		} catch (final IOException e) {
+			SNTUtils.error("Download failed", e);
+		}
+		return false;
+	}
 
 }
