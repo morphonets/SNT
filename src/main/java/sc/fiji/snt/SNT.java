morphonets/SNT

commit b1188d81431388d592615fff4e7a3b99789dfc2d

Minimize ij.dependencies (cont.) and reduce memory-persistent objects
- Reduce ij dependencies from CommonDynamicCmd and SNT classes
- Move more ij.IJ calls to ImpUtils
- SNT: Do not strore RAI as Dataset as it is never used directly
- Allow Cached data to be displayed from GUI after source image is closed

src/main/java/sc/fiji/snt/SNT.java
@@ -30,7 +30,6 @@ import ij.gui.*;
 import ij.measure.Calibration;
 import ij.process.ColorProcessor;
 import ij.process.ImageStatistics;
-import ij.process.LUT;
 import ij.process.ShortProcessor;
 import ij3d.Content;
 import ij3d.ContentConstants;
@@ -39,7 +38,6 @@ import ij3d.Image3DUniverse;
 import io.scif.services.DatasetIOService;
 import net.imagej.Dataset;
 import net.imagej.display.ColorTables;
-import net.imagej.lut.LUTService;
 import net.imagej.ops.OpService;
 import net.imagej.ops.special.computer.AbstractUnaryComputerOp;
 import net.imglib2.*;
@@ -67,7 +65,6 @@ import org.scijava.NullContextException;
 import org.scijava.app.StatusService;
 import org.scijava.command.CommandService;
 import org.scijava.convert.ConvertService;
-import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.util.ColorRGB;
 import org.scijava.vecmath.Color3f;
@@ -117,17 +114,13 @@ public class SNT extends MultiDThreePanes implements
 	@Parameter
 	private Context context;
 	@Parameter
+	private DatasetIOService datasetIOService;
+	@Parameter
 	protected StatusService statusService;
 	@Parameter
-	private LogService logService;
+	private ConvertService convertService;
 	@Parameter
-	protected DatasetIOService datasetIOService;
-	@Parameter
-	protected ConvertService convertService;
-	@Parameter
-	protected OpService opService;
-	@Parameter
-	protected LUTService lutService;
+	private OpService opService;
 
 	public enum SearchType {
 		ASTAR, NBASTAR;
@@ -243,10 +236,8 @@ public class SNT extends MultiDThreePanes implements
 	protected String spacing_units = SNTUtils.getSanitizedUnit(null);
 	protected int channel;
 	protected int frame;
-	private LUT lut;
 
 	/* all tracing and filling-related functions are performed on the Imgs */
-	Dataset dataset;
 	@SuppressWarnings("rawtypes")
 	RandomAccessibleInterval ctSlice3d;
 
@@ -528,7 +519,13 @@ public class SNT extends MultiDThreePanes implements
 	}
 
 	public boolean accessToValidImageData() {
-		return ctSlice3d != null; //getImagePlus() != null && !isDisplayCanvas(xy);
+		// NB: Currently we are assuming that image data comes from an ImagePlus. This
+		// needs to be changed when ImagePlus requirements are lift. Note that xy may
+		// be null (e.g., image has been manually closed), but its cached data ctSlice3d
+		// may still be available. Return ctSlice3d != null is problematic on its own,
+		// because there are several calls to this method that happen _before_ ctSlice3d
+		// has been assembled, but _after_ an image has been specified
+		return getImagePlus() != null && !isDisplayCanvas(xy);
 	}
 
 	private void setIsDisplayCanvas(final ImagePlus imp) {
@@ -537,6 +534,16 @@ public class SNT extends MultiDThreePanes implements
 
 	protected boolean isDisplayCanvas(final ImagePlus imp) {
 		return "SNT Display Canvas".equals(imp.getInfoProperty());
+	}
+
+	private void setIsCachedData(final ImagePlus imp) {
+		// NB: somehow setProperty/getProperty does not work with virtual stacks,
+		// so we'll brand the image title instead
+		imp.setTitle(String.format("Cached Data [C%dT%d]", channel, frame));
+	}
+
+	protected boolean isCachedData(final ImagePlus imp) {
+		return imp.getTitle().equals(String.format("Cached Data [C%dT%d]", channel, frame));
 	}
 
 	private void assembleDisplayCanvases() {
@@ -716,9 +723,8 @@ public class SNT extends MultiDThreePanes implements
 	@SuppressWarnings("unchecked")
 	private void loadDatasetFromImagePlus(final ImagePlus imp) {
 		statusService.showStatus("Loading data...");
-		this.dataset = convertService.convert(imp, Dataset.class);
-		this.ctSlice3d = ImgUtils.getCtSlice3d(this.dataset, channel - 1, frame - 1);
-		SNTUtils.log("Dataset dimensions: " + Arrays.toString(Intervals.dimensionsAsLongArray(dataset)));
+		this.ctSlice3d = ImgUtils.getCtSlice3d(imp, channel - 1, frame - 1);
+		SNTUtils.log("Dataset dimensions: " + Arrays.toString(imp.getDimensions()));
 		SNTUtils.log("CT HyperSlice dimensions: " + Arrays.toString(Intervals.dimensionsAsLongArray(this.ctSlice3d)));
 		statusService.showStatus("Finding stack minimum / maximum");
 		final boolean restoreROI = imp.getRoi() instanceof PointRoi;
@@ -729,7 +735,6 @@ public class SNT extends MultiDThreePanes implements
 			SNTUtils.log("Computing stack statistics");
 			computeImgStats(Views.iterable(this.ctSlice3d), getStats());
 		}
-		updateLut();
 	}
 
 	public void startUI() {
@@ -780,7 +785,7 @@ public class SNT extends MultiDThreePanes implements
 	}
 
 	public Dataset getDataset() {
-		return dataset;
+		return (ctSlice3d == null) ? null : convertService.convert(ctSlice3d, Dataset.class);
 	}
 
 	public ImagePlus getImagePlus() {
@@ -816,7 +821,6 @@ public class SNT extends MultiDThreePanes implements
 		ui = null;
 		flushSecondaryData();
 		ctSlice3d = null;
-		dataset = null;
 		//searchArtists.clear(); fillerSet.clear();
 		searchArtists = null;
 		fillerSet = null;
@@ -2583,21 +2587,19 @@ public class SNT extends MultiDThreePanes implements
 	 *         traced, or null if no image data has been loaded into memory.
 	 */
 	public <T extends RealType<T>> ImagePlus getLoadedDataAsImp() {
-		if (!accessToValidImageData())
+		if (ctSlice3d == null)
 			return null;
 		final RandomAccessibleInterval<T> data = getLoadedData();
-		final ImagePlus imp = ImgUtils.raiToImp(data, "Image");
-		updateLut();
-		imp.setLut(lut);
+		final ImagePlus imp = ImgUtils.raiToImp(data, "LoadedData");
 		imp.copyScale(xy);
 		imp.resetDisplayRange();
+		setIsCachedData(imp);
 		return imp;
 	}
 
+	@SuppressWarnings("unchecked")
 	public <T extends RealType<T>> RandomAccessibleInterval<T> getLoadedData() {
-		@SuppressWarnings("unchecked")
-		final RandomAccessibleInterval<T> data = Views.dropSingletonDimensions(this.ctSlice3d);
-		return data;
+		return (ctSlice3d == null) ? null : Views.dropSingletonDimensions(ctSlice3d);
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -2829,9 +2831,7 @@ public class SNT extends MultiDThreePanes implements
 		if (secondaryData.numDimensions() == 3) {
 			img = Views.permute(Views.addDimension(img, 0,0), 2,3);
 		}
-		ImagePlus imp = ImageJFunctions.wrap(img, "Secondary Layer");
-		updateLut();
-		imp.setLut(lut);
+		final ImagePlus imp = ImageJFunctions.wrap(img, "Secondary Layer");
 		imp.copyScale(xy);
 		imp.resetDisplayRange();
 		return imp;
@@ -2964,10 +2964,6 @@ public class SNT extends MultiDThreePanes implements
 			xz.getWindow().setVisible(visible);
 		if (zy != null && zy.getWindow() != null)
 			zy.getWindow().setVisible(visible);
-	}
-
-	protected ImagePlus getMainImagePlusWithoutChecks() {
-		return xy;
 	}
 
 	protected void error(final String msg) {
@@ -3324,11 +3320,6 @@ public class SNT extends MultiDThreePanes implements
 		return new ImagePlus[] { xy8, views[0], views[1] };
 	}
 
-	private void updateLut() {
-		final LUT[] luts = xy.getLuts(); // never null
-		if (luts.length > 0) lut = luts[channel - 1];
-	}
-
 	/**
 	 * Overlays a semi-transparent MIP (8-bit scaled) of the data being traced
 	 * over the tracing canvas(es). Does nothing if image is 2D. Note that with
@@ -3399,7 +3390,7 @@ public class SNT extends MultiDThreePanes implements
 	 */
 	public synchronized void enableSnapCursor(final boolean enable) {
 		final boolean validImage = accessToValidImageData();
-		final boolean isBinary = validImage && xy.getProcessor().isBinary();
+		final boolean isBinary = validImage && ImpUtils.isBinary(xy);
 		snapCursor = enable && validImage && !isBinary;
 		if (isUIready()) {
 			if (enable && !validImage) {

src/main/java/sc/fiji/snt/SNTUI.java
@@ -72,7 +72,6 @@ import ij3d.Content;
 import ij3d.ContentConstants;
 import ij3d.Image3DUniverse;
 import ij3d.ImageWindow3D;
-import net.imagej.Dataset;
 import sc.fiji.snt.analysis.SNTTable;
 import sc.fiji.snt.analysis.TreeAnalyzer;
 import sc.fiji.snt.analysis.sholl.ShollUtils;
@@ -87,6 +86,7 @@ import sc.fiji.snt.io.NeuroMorphoLoader;
 import sc.fiji.snt.io.WekaModelLoader;
 import sc.fiji.snt.plugin.*;
 import sc.fiji.snt.tracing.cost.OneMinusErf;
+import sc.fiji.snt.util.ImpUtils;
 import sc.fiji.snt.viewer.Viewer3D;
 
 import javax.swing.*;
@@ -1023,8 +1023,13 @@ public class SNTUI extends JDialog {
 		channelSpinner.setEnabled(hasChannels);
 		frameSpinner.setEnabled(hasFrames);
 		applyPositionButton.addActionListener(e -> {
-			if (!plugin.accessToValidImageData()) {
+			if (!plugin.accessToValidImageData() && plugin.getLoadedData() == null) {
 				guiUtils.error("There is no valid image data to be loaded.");
+				return;
+			}
+
+			if (!plugin.accessToValidImageData() && plugin.getLoadedData() != null) {
+				cachedDataFallbackPrompt();
 				return;
 			}
 			if (imgDimensionsChanged() && guiUtils.getConfirmation(
@@ -1041,6 +1046,16 @@ public class SNTUI extends JDialog {
 		positionPanel.add(applyPositionButton);
 		sourcePanel.add(positionPanel, gdb);
 		return sourcePanel;
+	}
+
+	private void cachedDataFallbackPrompt() {
+		final ImagePlus cached = plugin.getLoadedDataAsImp();
+		if (cached != null && guiUtils.getConfirmation("Image seems to have been closed. Recover cached data?",
+				"Original Image Closed", "Recover", "Ignore")) {
+			plugin.initialize(cached);
+			if (ImpUtils.isVirtualStack(cached))
+				guiUtils.error("Data was recovered as a virtual stack. Some functionality may not be available.");
+		}
 	}
 
 	private boolean imgDimensionsChanged() {
@@ -1781,13 +1796,12 @@ public class SNTUI extends JDialog {
 					return; // user pressed cancel
 				try {
 					plugin.statusService.showStatus(("Loading " + imageFile.getName()));
-					final Dataset ds = plugin.datasetIOService.open(imageFile.getAbsolutePath());
-					final ImagePlus colorImp = plugin.convertService.convert(ds, ImagePlus.class);
+					final ImagePlus colorImp = ImpUtils.open(imageFile);
 					showStatus("Applying color labels...", false);
 					plugin.setColorImage(colorImp);
 					showStatus("Labels image loaded...", true);
 
-				} catch (final IOException exc) {
+				} catch (final Exception exc) {
 					guiUtils.error("Could not open " + imageFile.getAbsolutePath() + ". Maybe it is not a valid image?",
 							"IO Error");
 					exc.printStackTrace();
@@ -3219,11 +3233,12 @@ public class SNTUI extends JDialog {
 			}
 
 			final String defaultText;
-			if (!plugin.accessToValidImageData()) {
+			if (!plugin.accessToValidImageData() || plugin.getImagePlus() == null) {
 				defaultText = "Image data unavailable...";
 			} else {
-				defaultText = "Tracing " + StringUtils.abbreviate(plugin.getImagePlus().getShortTitle(), 25) + ", C=" + plugin.channel + ", T="
-						+ plugin.frame;
+				defaultText = "Tracing "
+						+ StringUtils.abbreviate(plugin.getImagePlus().getShortTitle(), 25) + ", C="
+						+ plugin.channel + ", T=" + plugin.frame;
 			}
 
 			if (!validMsg) {
@@ -3395,8 +3410,13 @@ public class SNTUI extends JDialog {
 
 		final ImageWindow xy_window = (plugin.getImagePlus()==null) ? null : plugin.getImagePlus().getWindow();
 		if (xy_window == null) {
-			if (displayErrorOnFailure)
-				guiUtils.error("XY view is not available.");
+			if (displayErrorOnFailure) {
+				if (!plugin.accessToValidImageData() && plugin.getLoadedData() != null) {
+					cachedDataFallbackPrompt();
+					return;
+				} else
+					guiUtils.error("XY view is not available.");
+			}
 			return;
 		}
 
@@ -3877,13 +3897,12 @@ public class SNTUI extends JDialog {
 		/* ImageListener */
 		@Override
 		public void imageClosed(final ImagePlus imp) {
-			if (imp != plugin.getMainImagePlusWithoutChecks())
-				return;
-			if (!plugin.isDisplayCanvas(imp)) {
-				// the image being closed contained valid data 
+			if (plugin.isCachedData(imp)) {
+				plugin.ctSlice3d = null;
+			} else if (imp == plugin.getImagePlus() && !plugin.isDisplayCanvas(imp)) {
 				plugin.pauseTracing(true, false);
+				SwingUtilities.invokeLater(() -> updateRebuildCanvasButton());
 			}
-			SwingUtilities.invokeLater(() -> updateRebuildCanvasButton());
 		}
 
 		/*

src/main/java/sc/fiji/snt/gui/cmds/ChooseDatasetCmd.java
@@ -30,6 +30,8 @@ import java.util.List;
 
 import net.imagej.ImageJ;
 import sc.fiji.snt.SNTPrefs;
+import sc.fiji.snt.SNTUI;
+import sc.fiji.snt.gui.GuiUtils;
 import sc.fiji.snt.util.ImpUtils;
 
 import org.scijava.ItemVisibility;
@@ -79,7 +81,7 @@ public class ChooseDatasetCmd extends CommonDynamicCmd {
 			cancel();
 			return;
 		}
-		chosenImp = comvertInPlaceToCompositeAsNeeded(chosenImp);
+		chosenImp = convertInPlaceToCompositeAsNeeded(ui, chosenImp);
 		if (chosenImp.getType() == ImagePlus.COLOR_RGB) {
 			cancel();
 			return;
@@ -188,6 +190,16 @@ public class ChooseDatasetCmd extends CommonDynamicCmd {
 	public static ImagePlus getCurrentImage() {
 //		return legacyService.getImageMap().lookupImagePlus(imageDisplayService.getActiveImageDisplay());;
 		return WindowManager.getCurrentImage();
+	}
+
+	protected static ImagePlus convertInPlaceToCompositeAsNeeded(final SNTUI ui, final ImagePlus imp) {
+		if (imp != null && imp.getType() == ImagePlus.COLOR_RGB && new GuiUtils((ui==null)?imp.getWindow():ui).getConfirmation(
+				"RGB images are (intentionally) not supported by SNT. You can however convert " + imp.getTitle()
+						+ " to a multichannel image. Would you like to do it now? (Import will abort if you choose \"No\")",
+				"Convert to Multichannel?")) {
+			return ImpUtils.convertRGBtoComposite(imp);
+		}
+		return imp;
 	}
 
 	public static void main(final String... args) {

src/main/java/sc/fiji/snt/gui/cmds/CommonDynamicCmd.java
@@ -29,11 +29,8 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
-import ij.ImagePlus;
-import ij.plugin.CompositeConverter;
 import sc.fiji.snt.SNTService;
 import sc.fiji.snt.SNTUI;
-import sc.fiji.snt.gui.GuiUtils;
 import sc.fiji.snt.viewer.Viewer3D;
 import sc.fiji.snt.SNT;
 import sc.fiji.snt.SNTPrefs;
@@ -153,18 +150,6 @@ public class CommonDynamicCmd extends DynamicCommand {
 		snt.updateDisplayCanvases();
 		snt.updateAllViewers();
 		snt.getPrefs().setTemp(SNTPrefs.NO_IMAGE_ASSOCIATED_DATA, true);
-	}
-
-	protected ImagePlus comvertInPlaceToCompositeAsNeeded(ImagePlus imp) {
-		if (imp.getType() == ImagePlus.COLOR_RGB && new GuiUtils(ui).getConfirmation(
-				"RGB images are (intentionally) not supported. You can however convert " + imp.getTitle()
-						+ " to a multichannel image. Would you like to do it now? (Import will abort if you choose \"No\")",
-				"Convert to Multichannel?")) {
-			imp.hide();
-			imp = CompositeConverter.makeComposite(imp);
-			imp.show();
-		}
-		return imp;
 	}
 
 	@Override

src/main/java/sc/fiji/snt/gui/cmds/OpenDatasetCmd.java
@@ -39,9 +39,8 @@ import ij.ImagePlus;
  *
  * @author Tiago Ferreira
  */
-@Plugin(type = Command.class, initializer = "init",
-	label = "Change Tracing Image")
-public class OpenDatasetCmd extends CommonDynamicCmd implements Command {
+@Plugin(type = Command.class, initializer = "init", label = "Change Tracing Image")
+public class OpenDatasetCmd extends CommonDynamicCmd {
 
 //	@Parameter
 //	private DatasetIOService ioService;
@@ -63,7 +62,6 @@ public class OpenDatasetCmd extends CommonDynamicCmd implements Command {
 
 	@Override
 	public void run() {
-		final boolean redirect = ij.IJ.redirectingErrorMessages();
 		try {
 			// In theory, we should be able to use ioService but the
 			// following seems to always generate a virtual stack
@@ -71,9 +69,8 @@ public class OpenDatasetCmd extends CommonDynamicCmd implements Command {
 //			final ImagePlus imp = convertService.convert(ds, ImagePlus.class);
 //			snt.initialize(imp);
 			if (file != null) {
-				ij.IJ.redirectErrorMessages(true);
 				ImagePlus imp = ImpUtils.open(file);
-				imp = comvertInPlaceToCompositeAsNeeded(imp);
+				imp = ChooseDatasetCmd.convertInPlaceToCompositeAsNeeded(ui, imp);
 				if (imp.getType() != ImagePlus.COLOR_RGB) {
 					snt.initialize(imp);
 				}
@@ -87,7 +84,6 @@ public class OpenDatasetCmd extends CommonDynamicCmd implements Command {
 					+ "'Choose Tracing Image -> From Open Image...'.");
 			ex.printStackTrace();
 		} finally {
-			ij.IJ.redirectErrorMessages(redirect);
 			resetUI();
 		}
 	}

src/main/java/sc/fiji/snt/util/ImgUtils.java
@@ -236,7 +236,7 @@ public class ImgUtils
      * @param channelIndex the channel position, 0-indexed
      * @param frameIndex the time position, 0-indexed
      * @param <T>
-     * @return the view rai
+     * @return the view RAI
      */
     public static < T extends RealType< T > > RandomAccessibleInterval< T > getCtSlice( final Dataset dataset,
                                                                                         final int channelIndex,
@@ -263,6 +263,27 @@ public class ImgUtils
         // If Z is a singleton dimension, drop it
         return Views.dropSingletonDimensions( img );
     }
+
+    /**
+     * Get a view of the {@link ImagePlus} at the specified channel and frame.
+     *
+     * @param imp the input ImagePlus
+     * @param channel the channel position, 1-indexed (as per ImagePlus convention)
+     * @param frame the time position, 1-indexed (as per ImagePlus convention)
+     * @param <T>
+     * @return the view RAI
+     */
+	public static <T extends RealType<T>> RandomAccessibleInterval<T> getCtSlice3d(final ImagePlus imp, final int channel,
+			final int frame) {
+		RandomAccessibleInterval<T> img = ImgUtils.impToRealRai5d(imp);
+		// Extract the relevant part of the imp
+		img = Views.hyperSlice(img, 2, channel - 1);
+		img = Views.hyperSlice(img, 3, frame - 1);
+		// bump to 3D
+        if ( img.numDimensions() == 2 )
+        	img = Views.addDimension( img, 0, 0 );
+		return img;
+	}
 
     /**
      * Wrap an {@link ImagePlus} to a {@link RandomAccessibleInterval} such that the number of dimensions in

src/main/java/sc/fiji/snt/util/ImpUtils.java
@@ -35,6 +35,7 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.io.Opener;
+import ij.plugin.CompositeConverter;
 import ij.plugin.ContrastEnhancer;
 import ij.plugin.Duplicator;
 import ij.plugin.ImagesToStack;
@@ -106,14 +107,29 @@ public class ImpUtils {
 		}
 	}
 
+	public static ImagePlus convertRGBtoComposite(final ImagePlus imp) {
+		if (imp.getType() == ImagePlus.COLOR_RGB) {
+			imp.hide();
+			final boolean isShowing = imp.isVisible();
+			final ImagePlus res = CompositeConverter.makeComposite(imp);
+			imp.flush();
+			if (isShowing) res.show();
+			return res;
+		}
+		return imp;
+	}
+
 	public static ImagePlus open(final File file) {
 		return open(file, null);
 	}
 
 	public static ImagePlus open(final File file, final String title) {
+		final boolean redirecting = IJ.redirectingErrorMessages();
+		IJ.redirectErrorMessages(true);
 		final ImagePlus imp = IJ.openImage(file.getAbsolutePath());
 		if (title != null)
 			imp.setTitle(title);
+		IJ.redirectErrorMessages(redirecting);
 		return imp;
 	}
 
@@ -208,6 +224,14 @@ public class ImpUtils {
 	public static ImagePlus create(final String title, final int width, final int height, final int depth,
 			final int bitDepth) {
 		return ij.IJ.createImage(title, width, height, depth, bitDepth);
+	}
+
+	public static boolean isBinary(final ImagePlus imp) {
+		 return imp != null && imp.getProcessor() != null && imp.getProcessor().isBinary();
+	}
+
+	public static boolean isVirtualStack(final ImagePlus imp) {
+		 return imp != null && imp.getStack() != null && imp.getStack().size() > 1 && imp.getStack().isVirtual();
 	}
 
 	public static ImagePlus combineSkeletons(final Collection<Tree> trees) {

commit 8212a6c99dd2a55ccbeca1abf1297fb389bbf5a5

Fix 1-based CT indices (ImagePlus -> RAI)
This was a regression introduced in  b1188d81 (March 5, 2024, "Minimize ij.dependencies")

The issue is reported here:
https://forum.image.sc/t/snt-traces-neurites-on-incorrect-channel/119612
https://forum.image.sc/t/issues-with-scale-recognition-and-channel-mapping-in-snt-5-0-2/

src/main/java/sc/fiji/snt/SNT.java
@@ -890,7 +890,8 @@ public class SNT extends MultiDThreePanes implements
 	@SuppressWarnings("unchecked")
 	private void loadDatasetFromImagePlus(final ImagePlus imp) {
 		statusService.showStatus("Loading data...");
-		this.ctSlice3d = ImgUtils.getCtSlice3d(imp, channel - 1, frame - 1);
+		this.ctSlice3d = ImgUtils.getCtSlice3d(imp, channel, frame); // 1-index as per IJ convention.
+
 		SNTUtils.log("Dimensions of input dataset [W,H,C,Z,T]: " + Arrays.toString(imp.getDimensions()));
 		SNTUtils.log(String.format("Dimensions:of imported XYZ volume (C=%d,T=%d): %s", channel, frame,
 				Arrays.toString(Intervals.dimensionsAsLongArray(this.ctSlice3d))));